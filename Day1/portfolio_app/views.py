6efb706f43de9ba9335f57d9ffc251c0d359f451
Adnannewb <rahadnan6@gmail.com>
2026-05-14 11:23:11 +0600

    day1

---
+++ Day1/portfolio_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request,'portfolio_app/home.html')
+
+def about(request):
+    return render(request, 'portfolio_app/about.html')
+
+def contact(request):
+    return render(request, 'portfolio_app/contact.html')
